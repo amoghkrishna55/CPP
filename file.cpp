a41af13cde2463ab3eb596f41632d0b4b3c291e9
Amogh Krishna <amoghkrishna55@gmail.com>
2023-09-11 20:29:31 +0530

    11th September

---
+++ file.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+  ofstream onFile;
+  string str;
+  onFile.open("prac1.txt");
+  getline(cin,str);
+  onFile << str;
+
+  onFile.close();
+
+  ifstream inFile;
+  string str1;
+  inFile.open("prac1.txt");
+  while(getline(inFile,str1)){
+    cout << str1;
+  }
+
+
+  inFile.close();
+}
