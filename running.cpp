3af3c06e0ef150abdf5a85181ff3ce68cecbab91
Amogh Krishna <amoghkrishna55@gmail.com>
2024-10-22 10:20:01 +0530

    Refactor running.cpp to improve code readability and maintainability

---
+++ running.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c << endl;
+    cout << a + 10 << " " << b + 100 << " " << c << endl;
+}
